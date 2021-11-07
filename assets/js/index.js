5552b1edf598f9e28bf65f04c6764dd292096d84
SergeyVishnevskiy <serrzik13manager@gmail.com>
2021-11-07 23:00:56 +0200

    index

---
+++ assets/js/index.js
@@ -0,0 +1,12 @@
+let vh = window.innerHeight * 0.01
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`)
+
+$(document).ready(function () {
+  $('.anchor').on('click', function (event) {
+    event.preventDefault()
+    var id = $(this).attr('href'),
+      top = $(id).offset().top
+    $('body,html').animate({ scrollTop: top }, 700)
+  })
+})
