6b9334b275a8c0b49ecc243c9df061a82144e656
SergeyVishnevskiy <serrzik13manager@gmail.com>
2021-11-08 15:19:01 +0200

    index

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,4 +9,14 @@ $(document).ready(function () {
       top = $(id).offset().top
     $('body,html').animate({ scrollTop: top }, 700)
   })
+
+  $('.tabs-triggers__item').click(function (e) {
+    e.preventDefault()
+
+    $('.tabs-triggers__item').removeClass('tabs-triggers__item-active')
+    $('.tabs-content__item').removeClass('tabs-content__item-active')
+
+    $(this).addClass('tabs-triggers__item-active')
+    $($(this).attr('href')).addClass('tabs-content__item-active')
+  })
 })
